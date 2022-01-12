bc63a9fb726aae0c3e17e234683ad9cbccc23ba7
weiwei <weittl@hotmail.com>
2022-01-12 18:34:41 +0800

    create deploy

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? './'
+      : './',
+    outputDir: "dist",
+    lintOnSave: true,
+    runtimeCompiler: false,
+    productionSourceMap: false,
+    devServer: {
+      open: process.platform === "darwin",
+      port: 8000,
+      https: false,
+      hotOnly: false,
+      proxy: null,
+      disableHostCheck: true,
+    }
+  };
